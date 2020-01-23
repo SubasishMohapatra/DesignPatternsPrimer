492e16e0d9a5202ca7b8a1df27c3ab75623091ca
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2020-01-23 18:30:15 +0530

    Add project files.

---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DotNetFactory().GetDeskTopDevelopers().SayHi();
+            new DotNetFactory().GetWebDevelopers().SayHi();
+            new DotNetFactory().GetMobileDevelopers().SayHi();
+            new JavaFactory().GetDeskTopDevelopers().SayHi();
+            new JavaFactory().GetMobileDevelopers().SayHi();
+            new JavaFactory().GetWebDevelopers().SayHi();
+        }
+    }
+
+    interface IDotNetFactory
+    {
+        IDesktopDeveloper GetDeskTopDevelopers();
+        IWebDeveloper GetWebDevelopers();
+        IMobileDeveloper GetMobileDevelopers();
+    }
+
+    public class DotNetFactory : IDotNetFactory
+    {
+        public IDesktopDeveloper GetDeskTopDevelopers()
+        {
+            return new DotNetDesktopDeveloper();
+        }
+
+        public IMobileDeveloper GetMobileDevelopers()
+        {
+            return new DotNetMobileDeveloper();
+        }
+
+        public IWebDeveloper GetWebDevelopers()
+        {
+            return new DotNetWebDeveloper();
+        }
+    }
+
+    public interface IWebDeveloper
+    {
+        void SayHi();
+    }
+
+    public interface IMobileDeveloper
+    {
+        void SayHi();
+    }
+
+    public interface IDesktopDeveloper
+    {
+        void SayHi();
+    }
+
+    interface IJavaFactory
+    {
+        IDesktopDeveloper GetDeskTopDevelopers();
+        IWebDeveloper GetWebDevelopers();
+        IMobileDeveloper GetMobileDevelopers();
+    }
+
+    public class JavaFactory : IJavaFactory
+    {
+        public IDesktopDeveloper GetDeskTopDevelopers()
+        {
+            return new JavaDesktopDeveloper();
+        }
+
+        public IMobileDeveloper GetMobileDevelopers()
+        {
+            return new JavaMobileDeveloper();
+        }
+
+        public IWebDeveloper GetWebDevelopers()
+        {
+            return new JavaWebDeveloper();
+        }
+    }
+    public class DotNetDesktopDeveloper:IDesktopDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Dotnet desktop developer at your service sir");
+        }
+    }
+    public class DotNetWebDeveloper : IWebDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Dotnet web developer at your service sir");
+        }
+    }
+    public class DotNetMobileDeveloper : IMobileDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Dotnet mobile developer at your service sir");
+        }
+    }
+    public class JavaDesktopDeveloper : IDesktopDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Java desktop developer at your service sir");
+        }
+    }
+    public class JavaWebDeveloper : IWebDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Java web developer at your service sir");
+        }
+    }
+    public class JavaMobileDeveloper : IMobileDeveloper
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi!!! Java mobile developer at your service sir");
+        }
+    }
+
+}
---
+++ FactoryPattern/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new DotNetResourceFacory().GetProfessional().SayHi();
+            new JavaResourceFacory().GetProfessional().SayHi();
+        }
+    }
+
+    public interface IResource
+    {
+        void SayHi();
+    }
+
+    public interface IResourceFactory
+    {
+        public IResource GetProfessional();
+    }
+
+    public class DotNetResourceFacory : IResourceFactory
+    {
+        public IResource GetProfessional()
+        {
+            return new DotNetResource();
+        }
+    }
+
+    public class JavaResourceFacory : IResourceFactory
+    {
+        public IResource GetProfessional()
+        {
+            return new JavaResource();
+        }
+    }
+
+    public class DotNetResource : IResource
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi !!! I am a dotnet resource");
+        }
+    }
+
+    public class JavaResource : IResource
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi !!! I am a java resource");
+        }
+    }
+}
